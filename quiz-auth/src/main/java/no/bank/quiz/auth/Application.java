b3b2a4cccaf7ef6a426871f553ddc621f4e07c62
Bentein Thomassen <benteinvathnethomassen@gmail.com>
2019-01-03 14:19:11 +0100

    added authorization server

---
+++ quiz-auth/src/main/java/no/bank/quiz/auth/Application.java
@@ -0,0 +1,14 @@
+package no.bank.quiz.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@SpringBootApplication
+@EnableAuthorizationServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ quiz-auth/src/main/java/no/bank/quiz/auth/config/AuthServerConfig.java
@@ -0,0 +1,20 @@
+package no.bank.quiz.auth.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+@Configuration
+public class AuthServerConfig  {
+
+    @Value("${security.oauth2.client.signingKey}")
+    private String signingKey;
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(signingKey);
+        return converter;
+    }
+}
